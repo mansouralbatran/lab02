84b1a35f04d63d0e0c80c731a760201014c9e1c7
mansouralbatran <mansouralbatran@gmail.com>
2021-06-21 17:43:46 +0300

    my pag

---
+++ js/app.js
@@ -0,0 +1,106 @@
+'use strict';
+
+let ans1 = prompt("do yo expect that i am tall some one").toUpperCase()
+
+switch (ans1) {
+    case "YES":
+        console.log("you right");
+        alert("you right");
+
+        break;
+    case "NO":
+        console.log("rong");
+        alert("rong")
+        break;
+    default:
+
+        console.log("please answer by yes or no");
+
+        alert("please answer by yes or no")
+        break
+}
+
+
+
+
+
+let ans2 = prompt("do you think that i like foot ball or no").toUpperCase()
+switch (ans2) {
+    case "YES":
+        console.log("you right");
+        alert("you right");
+
+        break;
+    case "NO":
+        console.log("rong");
+        alert("rong")
+        break;
+    default:
+
+        console.log("please answer by yes or no");
+
+        alert("please answer by yes or no")
+        break
+}
+
+
+let ans3 = prompt("do youthink i live in jordan").toUpperCase()
+switch (ans3) {
+    case "YES":
+        console.log("you right");
+        alert("you right");
+
+        break;
+    case "NO":
+        console.log("rong");
+        alert("rong")
+        break;
+    default:
+
+        console.log("please answer by yes or no");
+
+        alert("please answer by yes or no")
+        break
+}
+
+
+let ans4 = prompt("do you think that i like my major").toUpperCase()
+switch (ans4) {
+    case "YES":
+        console.log("you right");
+        alert("you right");
+
+        break;
+    case "NO":
+        console.log("rong");
+        alert("rong")
+        break;
+    default:
+
+        console.log("please answer by yes or no");
+
+        alert("please answer by yes or no")
+        break
+}
+
+
+let ans5 = prompt("do you think that I like shawromaa").toUpperCase()
+switch (ans5) {
+    case "YES":
+        console.log("you right");
+        alert("you right");
+
+        break;
+    case "NO":
+        console.log("rong");
+        alert("rong")
+        break;
+    default:
+
+        console.log("please answer by yes or no");
+
+        alert("please answer by yes or no")
+        break
+}
+let userName = prompt("what is your nam")
+alert("welcome" + userName)
