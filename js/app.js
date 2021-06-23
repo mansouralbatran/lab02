9b18cbc4c239ca6df603cdcef9e0cb8542486aa5
abdullahnazzal <a.nazzal1995@gmail.com>
2021-06-23 14:59:46 +0300

    added function by Abdullah

--- js/app.js
+++ js/app.js
@@ -1,151 +1,153 @@
 /* eslint-disable eol-last */
 'use strict';
-let x =0;
-let ans1 = prompt('do yo expect that i am tall some one').toUpperCase();
+let scorel = 0;
+let answer1 = "Yes".toLowerCase();
+let answer2 = "Y".toLowerCase();
 
-switch (ans1) {
-case 'YES':
-  console.log('you right');
-  alert('you right');
-  x =x +1;
-  break;
-case 'NO':
-  console.log('rong');
-  alert('rong');
-  break;
-default:
+let userName = prompt('what is your nam: ');
+alert('welcome ' + userName);
 
-  console.log('please answer by yes or no');
+function q1() {
+  let ans1 = prompt('do yo expect that i am tall some one').toLowerCase();
+  switch (ans1) {
+    case answer1:
+    case answer2:
+      console.log('you right');
+      alert('you right');
+      scorel = scorel + 1;
+      break;
 
-  alert('please answer by yes or no');
-  break;
-}
-
-
-
-
-
-let ans2 = prompt('do you think that i like foot ball or no').toUpperCase();
-switch (ans2) {
-case 'YES':
-  console.log('you right');
-  alert('you right');
-  x = x +1;
-  break;
-case 'NO':
-  console.log('rong');
-  alert('rong');
-  break;
-default:
-
-  console.log('please answer by yes or no');
-
-  alert('please answer by yes or no');
-  break;
-}
-
-
-let ans3 = prompt('do youthink i live in jordan').toUpperCase();
-switch (ans3) {
-case 'YES':
-  console.log('you right');
-  alert('you right');
-  x = x +1;
-  break;
-case 'NO':
-  console.log('rong');
-  alert('rong');
-  break;
-default:
-
-  console.log('please answer by yes or no');
-
-  alert('please answer by yes or no');
-  break;
-}
-
-
-let ans4 = prompt('do you think that i like my major').toUpperCase();
-switch (ans4) {
-case 'YES':
-  console.log('you right');
-  alert('you right');
-  x = x +1;
-  break;
-case 'NO':
-  console.log('rong');
-  alert('rong');
-  break;
-default:
-
-  console.log('please answer by yes or no');
-
-  alert('please answer by yes or no');
-  break;
-}
-
-
-let ans5 = prompt('do you think that I like shawromaa').toUpperCase();
-switch (ans5) {
-case 'YES':
-  console.log('you right');
-  alert('you right');
-  x = x +1;
-  break;
-case 'NO':
-  console.log('rong');
-  alert('rong');
-  break;
-default:
-
-  console.log('please answer by yes or no');
-
-  alert('please answer by yes or no');
-  break;
-}
-let userName = prompt('what is your nam');
-alert('welcome' + userName);
-
-
-
-let ques5 = prompt('How many times have I traveled by plane');
-
-for (let i = 0; i < 4; i++) {
-  if (ques5 === '5') {
-    alert('you get it');
-    x = x +1;
-    break;
+    default:
+      console.log('wrong');
+      alert('wrong');
+      break;
   }
-  else if (ques5 > '5') {
-    alert('no that to much');
-    ques5 = prompt('How many times have I traveled by plane');
+
+}
+
+
+
+
+
+let funQ2 = function q2() {
+
+  let ans2 = prompt('do you think that i like foot ball or no').toLowerCase();
+  switch (ans2) {
+    case answer1:
+    case answer2:
+      console.log('you right');
+      alert('you right');
+      scorel = scorel + 1;
+      break;
+    default:
+      console.log('wrong');
+      alert('wrong');
+      break;
   }
-  else if (ques5 < '5') {
-    alert('that is less');
-    ques5 = prompt('How many times have I traveled by plane');
-  }
-  if (i === 3) {
-    alert('the correct answer is 5');
+
+}
+
+
+function q3() {
+  let ans3 = prompt('do youthink i live in jordan').toLowerCase();
+  switch (ans3) {
+    case answer1:
+    case answer2:
+      console.log('you right');
+      alert('you right');
+      scorel = scorel + 1;
+      break;
+
+    default:
+      console.log('wrong');
+      alert('wrong');
+      break;
+
   }
 }
 
-let favNUM = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
-let ques7 = prompt('Guess one of tow from my favort number from this list' + favNUM);
 
-for (let i = 0; i < 6; i++) {
-  if (ques7 === '7') {
-    alert('thats great the corect answer(3,7)');
-    x = x +1;
-    break;
-  } else if (ques7 === '3') {
-    alert('thats great the corect answer(3,7');
-    x = x +1;
-    break;
+function q4() {
+  let ans4 = prompt('do you think that i like my major').toLowerCase();
+  switch (ans4) {
+
+    case answer1:
+    case answer2:
+      console.log('you right');
+      alert('you right');
+      scorel = scorel + 1;
+      break;
+    default:
+
+      console.log('rong');
+      alert('rong');
+      break;
+
   }
-  else {
-    alert('that is wrong keep trying');
-    ques7 = prompt('Guess how many brothers I have from this list' + favNUM);
-  } if (i === 5) { alert('the corect answer(3,7)'); }
+
 }
-console.log(x);
-alert('you have ' + x + 'correct answer');
+
+function q5() {
+
+  let ans5 = prompt('do you think that I like shawromaa').toLowerCase();
+  switch (ans5) {
+    case answer1:
+    case answer2:
+      console.log('you right');
+      alert('you right');
+      scorel = scorel + 1;
+      break;
+    default:
+
+      console.log('wrong');
+      alert('wrong');
+      break;
+  }
+}
+function q6() {
+  for (let i = 0; i < 4; i++) {
+    let ques5 = Number(prompt('How many times have I traveled by plane'));
+    if (ques5 === 5) {
+      alert('you get it');
+      scorel = scorel + 1;
+      break;
+    }
+    else if (ques5 > 5) {
+      alert('no that to much');
+      // continue;
+    }
+    else if (ques5 < 5) {
+      alert('that is less');
+      // continue;
+
+    }
+  }
+}
+
+let funQ7 = function q7() {
+  let flag=false;
+  let cars = ["kia", "bmw", "MC", "tesla", "mazda"];
+  for (let i = 0; i < 6; i++) {
+    if(flag==false){
+      let ques7 = prompt("Guess one of tow from my favort Car :");
+    for (let j = 0; j < cars.length; j++) {
+      if (ques7 == cars[j]) {
+        alert('you get it');
+        scorel = scorel + 1;
+        flag=true;
+        break;
+      }
+    }
+    }
+  }
+}
+q1();
+funQ2();
+q3();
+q4();
+q5();
+q6();
+funQ7();
+
+ console.log(scorel);
+ alert('you have ' + scorel + ' correct answer');
